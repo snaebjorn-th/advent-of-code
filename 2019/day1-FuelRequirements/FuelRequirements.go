6fc95e9881b5cf81ac36674f20c0e806b4fe2fcf
Snaebjorn Eyjolfsson <snabbie1@gmail.com>
2019-12-10 17:52:19 +0000

    refactored day1

--- 2019/day1-FuelRequirements/FuelRequirements.go
+++ 2019/day1-FuelRequirements/FuelRequirements.go
@@ -1,64 +1,37 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io/ioutil"
-	"os"
-	"strconv"
-	"strings"
-	"unicode"
+
+	"github.com/snaebjorn-th/advent-of-code/utils"
 )
 
-// ReadInput returns the contents of filename as a string.
-func ReadInput(filename string) string {
-	data, err := ioutil.ReadFile(filename)
-	if err != nil {
-		panic(err)
-	}
-	return string(bytes.TrimSpace(data))
-}
-
-func ExtractInts(s string) []int {
-	fs := strings.FieldsFunc(s, func(r rune) bool {
-		return !unicode.IsDigit(r) && r != '-'
-	})
-	ints := make([]int, 0, len(fs))
-	for _, w := range fs {
-		i, err := strconv.Atoi(w)
-		if err == nil {
-			ints = append(ints, i)
-		}
-	}
-	return ints
-}
-
-func FuelRequired(mass int) int {
+func fuelRequired(mass int) int {
 	return (mass / 3) - 2
 }
 
-func FuelRequiredRecursive(mass int) int {
-	mass = FuelRequired(mass)
+func fuelRequiredRecursive(mass int) int {
+	mass = fuelRequired(mass)
 	if mass < 0 {
 		return 0
 	}
-	return mass + FuelRequiredRecursive(mass)
+	return mass + fuelRequiredRecursive(mass)
 }
 
 func main() {
-	var input = ReadInput(os.Args[1])
-	var numbers = ExtractInts(input)
+	var input = utils.ReadInput(utils.GetFilename())
+	var numbers = utils.ExtractInts(input)
 
 	var sum = 0
 	for _, num := range numbers {
-		sum += FuelRequired(num)
+		sum += fuelRequired(num)
 	}
 
 	fmt.Printf("Part 1: %d\n", sum)
 
 	var sum2 = 0
 	for _, num := range numbers {
-		sum2 += FuelRequiredRecursive(num)
+		sum2 += fuelRequiredRecursive(num)
 	}
 
 	fmt.Printf("Part 2: %d\n", sum2)
